3d3f962d3a81e88d2b67bf8a5f75bcbbfd6c26ee
Serji2158 <sergey.tyschenko@gmail.com>
2022-01-02 18:49:17 +0200

    algs added

--- algs.js/binary_search.js
+++ algs.js/binary_search.js
@@ -26,3 +26,16 @@ function binarySearch(array, item) {
 
 console.log(binarySearch(array, 11));
 console.log(count);
+
+function recursiveBinarySearch(array, item, start, end) {
+  let middle = Math.floor((start + end) / 2);
+  if (item === array[middle]) {
+    return middle;
+  }
+  if (item < array[middle]) {
+    return recursiveBinarySearch(array, item, start, middle - 1);
+  } else {
+    return recursiveBinarySearch(array, item, middle + 1, end);
+  }
+}
+console.log(recursiveBinarySearch(array, 0, 0, array.length));
